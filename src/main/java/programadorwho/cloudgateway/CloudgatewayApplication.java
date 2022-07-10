fb6328e792945208a62dcf29c01fcca99294d4f8
Thiago <thiagoandrecardoso@icloud.com>
2022-07-10 19:59:25 -0300

    initial commit

---
+++ src/main/java/programadorwho/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,28 @@
+package programadorwho.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclients"))
+				.build();
+	}
+
+}
